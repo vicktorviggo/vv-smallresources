5b420f1bb0830795b1c9b2db9cc3625aa2d71a64
Vicktor <133223399+vicktorviggo@users.noreply.github.com>
2025-06-21 00:10:57 +0200

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+author 'vicktorviggo'
+description 'a lot of different small stuff for FiveM'
+
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'popgroups.ymt'
+
+files {
+    'popgroups.ymt',
+    'relationships.dat'
+}
